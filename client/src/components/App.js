0851fdde8b286d76f5a07343e40c035443a45758
Rocky Cifu <rocky.cifu@launchacademy.co>
2022-03-08 15:09:28 -0800

    a6-final-code

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,60 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { hot } from "react-hot-loader/root"
+
+import MoviesIndex from "./MoviesIndex"
+import MovieForm from "./MovieForm"
 
 import "../assets/scss/main.scss"
 
-const App = props => {
+const App = (props) => {
+  const [movies, setMovies] = useState([])
+
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch("/api/v1/movies")
+
+      if (!response.ok) {
+        const message = `${response.status} (${response.statusText})`
+        throw new Error(message)
+      }
+
+      const moviesData = await response.json()
+      setMovies(moviesData.movies)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const addMovie = async (newMovie) => {
+    try {
+      const response = await fetch("/api/v1/movies", {
+        method: "POST",
+        headers: new Headers({ "Content-Type": "application/json" }),
+        body: JSON.stringify({ movie: newMovie }),
+      })
+
+      if (!response.ok) {
+        const message = `${response.status} (${response.statusText})`
+        throw new Error(message)
+      }
+
+      const movieData = await response.json()
+
+      setMovies([...movies, movieData.newMovie])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
   return (
     <div className="app">
       <h1>My Favorite Disney Movies</h1>
+      <MovieForm addMovie={addMovie} />
+      <MoviesIndex movies={movies} />
     </div>
   )
 }
--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -1,21 +1,67 @@
-import React from "react"
+import React, { useState } from "react"
 
-const MovieForm = props => {
+const MovieForm = (props) => {
+  const [values, setValues] = useState({
+    title: "",
+    releaseYear: "",
+    runtime: "",
+  })
+
+  const handleChange = (event) => {
+    event.preventDefault()
+    setValues({ ...values, [event.target.name]: event.target.value })
+  }
+
+  const handleClear = () => {
+    setValues({
+      title: "",
+      releaseYear: "",
+      runtime: "",
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    props.addMovie(values)
+    handleClear(event)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor="title">Title:</label>
-      <input id="title" type="text" />
+      <input
+        id="title"
+        type="text"
+        name="title"
+        value={values.title}
+        onChange={handleChange}
+      />
       <br />
 
       <label htmlFor="releaseYear">Release Year:</label>
-      <input id="releaseYear" type="text" />
+      <input
+        id="releaseYear"
+        type="text"
+        name="releaseYear"
+        value={values.releaseYear}
+        onChange={handleChange}
+      />
       <br />
 
       <label htmlFor="runtime">Runtime (minutes):</label>
-      <input id="runtime" type="text" />
+      <input
+        id="runtime"
+        type="text"
+        name="runtime"
+        value={values.runtime}
+        onChange={handleChange}
+      />
       <br />
 
       <input type="submit" value="Add this Movie" />
+      <button type="button" onClick={handleClear}>
+        Clear Form
+      </button>
     </form>
   )
 }
